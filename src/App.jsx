c781af5ba3ddb15756e0a455e8887730da7eafe4
IT STUD <olaabdulsataar@gmail.com>
2026-05-03 09:47:51 +0100

    add theme storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -6,8 +6,16 @@ import Router from "./components/Routes";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [darkTheme, setDarkTheme] = useState(false);
-  return (
+  const [darkTheme, setDarkTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("darkTheme");
+    return savedTheme ? JSON.parse(savedTheme) : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkTheme", JSON.stringify(darkTheme));
+  }, [darkTheme]);
+
+  return(
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-gray-50 dark:bg-violet-950 dark:text-violet-100 min-h-screen flex flex-col">
         <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
